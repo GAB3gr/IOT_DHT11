3dc1b0c6c90f7e607b4c0867b075e3d0e8fa51c0
GAB3gr <gregorioserver@gmail.com>
2025-10-30 18:56:45 -0300

    iot3.0

--- index.js
+++ index.js
@@ -2,24 +2,25 @@ const { SerialPort } = require("serialport");
 const { ReadlineParser } = require("@serialport/parser-readline");
 const mqtt = require("mqtt");
 
-const port = new SerialPort({ path: "COM5", baudRate: 9600});
+// Serial Arduino
+const port = new SerialPort({ path: "COM5", baudRate: 9600 });
 const parser = port.pipe(new ReadlineParser({ delimiter: "\n" }));
 
-const client = mqtt.connect("mqtt://broker.hivemq.com:1883");
-const topic = "senai/iot/dht11";
+// MQTT via WebSocket
+const client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt"); // WebSocket público HiveMQ
+const topicDados = "senai/iot/dht11";
 
-client.on("connect", () => {
-    console.log("Conectado ao broker MQTT!");
+client.on("connect", ()=>{
+  console.log("Conectado MQTT!");
 });
 
-parser.on("data", (line) => {
-    try {
-        const data = JSON.parse(line.trim());
-        console.log("Recebido: ", data);
-
-        client.publish(topic, JSON.stringify(data));
-        console.log("Publicado no MQTT", data);
-    } catch (error) {
-        console.log("Erro ao parsear: ", line);
-    }
+// Recebe dados do Arduino via Serial e publica no MQTT
+parser.on("data", (line)=>{
+  try{
+    const data = JSON.parse(line.trim());
+    console.log("Recebido Arduino:", data);
+    client.publish(topicDados, JSON.stringify(data));
+  }catch(e){
+    console.log("Erro parse:", line);
+  }
 });
